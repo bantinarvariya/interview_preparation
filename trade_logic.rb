3d67dd7c2c610776b9bdf9725c86a3302c4b86c0
Banti Narvariya <bantinarvariya16@gmail.com>
2024-03-24 22:25:18 +0530

    minor changes

--- trade_logic.rb
+++ trade_logic.rb
@@ -1,24 +1,38 @@
-# Interview question
+# Interview question that never asked.
 
-def cc(intitial, rate)
-	loss = 0
-	profit = 0
-	(0..5).each_with_index do |v , i|
-		profit = (intitial.to_f/100.0)*rate.to_f
-		l = (profit/2.0)
-		loss = loss + (profit/2.0)
-		puts "-------------------------------------------"
-		puts "Intitial #{intitial}Rs"
-		puts "Profit in this trade #{profit}Rs -> #{rate}%"
-		puts "This trade Loss #{l}Rs -> #{(rate.to_f/2.0)}%"
-		puts "Overall loss before this trade #{l}Rs -> #{(rate.to_f/2.0)}%"
-		puts "Net Loss After this trade #{loss}Rs -> #{(rate.to_f/2.0)}%"
-		puts "Net Profit After this trade #{l}Rs #{(rate.to_f/2.0)}%"
-		intitial = loss.to_f/(rate.to_f/2.0)*100
-		puts "-------------------------------------------"
-		puts
-	end
+def cc(intitial, target, stoploss)
+  loss = 0
+  if stoploss > target
+    puts "#{stoploss} can't be greater than #{target}"
+    return
+  end
+  if intitial.negative?
+    puts "#{intitial} can't be nagative"
+    return
+  end
+  while true
+    profit = (intitial.to_f/100.0)*target.to_f
+    l = (intitial.to_f/100.0)*stoploss.to_f
+    previes_loss = loss
+    net_profit = profit - previes_loss
+    if net_profit.negative?
+      puts "Net Profit can't be nagative"
+      break
+    end
+    loss = loss + l
+    puts "------------------------------------------------------------------"
+    puts "Intitial #{intitial.round(2)}Rs"
+    puts "Profit in this trade                 #{profit.round(2)}Rs -> #{target}%"
+    puts "This trade Loss                      #{l.round(2)}Rs -> #{(stoploss)}%"
+    puts "Overall loss before this trade       #{previes_loss.round(2)}Rs"
+    puts "Net Profit After this trade          #{net_profit.round(2)}Rs"
+    puts "Net Loss After this trade            #{loss.round(2)}Rs"
+    puts "------------------------------------------------------------------"
+    puts
+    intitial = loss.to_f/(stoploss)*90 # Always cover 90%
+    break if intitial > 100000
+  end
 end 
 
-cc(10000, 5)
+cc(4000, 10, 5)
 
